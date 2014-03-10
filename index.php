9b84d6f2e02b101fa784630305ab4145f089ea9d
strivegfx <strivegfx@gmail.com>
2014-03-10 19:23:55 +1300

    v0.1
    - Created base HTML / CSS responsive structure

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/ht5bp.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="background"></div>
+
+        <div class="landing-frame">
+            <div class="logo">
+                <div class="fill">
+                    <img src="img/main-logo.svg">
+                </div>
+            </div>
+            <div class="icon">
+                <div class="fill"></div>
+            </div>
+            <div class="shadow"></div>
+        </div> <!-- end of main logo container -->
+
+        <nav class="scroll">
+            <ul>
+                <li class="hello"></li>
+                <li class="work"></li>
+                <li class="talk"></li>
+            </ul>
+        </nav>
+
+        <div class="hello module-container clear-fix">
+            <h2>hello</h2>
+            <div class="intro">
+                <p> <!-- constrain max width -->
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco.
+                </p>
+            </div>
+            <ul class="points clear-fix">
+                <li>
+                    <div class="icon design"></div>
+                    <div class="description">
+                        <h3>Design</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <div class="icon development"></div>
+                    <div class="description">
+                        <h3>Development</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <div class="icon dedication"></div>
+                    <div class="description">
+                        <h3>Dedication</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </div> <!-- end of hello container -->
+
+        <div class="work module-container">
+            <h2>work</h2>
+            <ul class="tiles clear-fix">
+                <li>
+                    <img class="spacer" src="img/spacer.png">
+                </li>
+                <li>
+                    <img class="spacer" src="img/spacer.png">
+                </li>
+                <li>
+                    <img class="spacer" src="img/spacer.png">
+                </li>
+                <li>
+                    <img class="spacer" src="img/spacer.png">
+                </li>
+                <li>
+                    <img class="spacer" src="img/spacer.png">
+                </li>
+                <li>
+                    <img class="spacer" src="img/spacer.png">
+                </li>
+            </ul>
+        </div> <!-- end of work container -->
+
+        <div class="talk module-container">
+            <h2>talk?</h2>
+            SOME CONTENT
+        </div> <!-- end of talk-container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
